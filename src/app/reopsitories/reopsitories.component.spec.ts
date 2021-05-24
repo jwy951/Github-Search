8fc28d53b53641b0038880a9131757c17ab6ecb5
Santa claus <santa@nortpole.com>
2021-05-24 22:53:16 +0300

    add not found 404

--- src/app/reopsitories/reopsitories.component.spec.ts
+++ src/app/reopsitories/reopsitories.component.spec.ts
@@ -19,7 +19,7 @@ describe('ReopsitoriesComponent', () => {
     fixture.detectChanges();
   });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
